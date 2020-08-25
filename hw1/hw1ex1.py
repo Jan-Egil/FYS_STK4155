08c84dfc23ff45a9082aad6a1304eff2a80d4ccd
Jan Egil Ødegård <janegil.ode@gmail.com>
2020-08-25 22:11:39 +0200

    Exercise 1 on hw1 completed

--- hw1/hw1ex1.py
+++ hw1/hw1ex1.py
@@ -4,6 +4,16 @@ import pandas as pd
 
 #file = open('EoS.csv','r')
 
-EoS = pd.read_csv('EoS.csv')
+EoS = pd.read_csv('EoS.csv',names=["Density","Energy"])
 
-print(EoS)
+rho = EoS['Density']
+
+n = len(rho) #Number of datapoints
+p = 4 #To define polynomial of degree p-1
+X = np.zeros([n,p])
+
+X[:,0] = 1
+X[:,1] = rho
+X[:,2] = rho**2
+X[:,3] = rho**3
+print(X)
