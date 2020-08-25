7bdc2b8398c53d636a583535c545a422bd5ddaca
Jan Egil Ødegård <janegil.ode@gmail.com>
2020-08-25 22:11:39 +0200

    Written out the final parts of ex2.

--- hw1/hw1ex2.py
+++ hw1/hw1ex2.py
@@ -2,9 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.model_selection import train_test_split
+import sklearn.metrics as metric
 
 x = np.random.rand(100,1)
-y = 5*x**2+2+0.1*np.random.randn(100,1)
+y = 5*x**2+2+0.5*np.random.randn(100,1)
 
 parametrization = np.polyfit(x[:,0],y[:,0],2)
 print(parametrization)
@@ -19,10 +20,31 @@ X = np.zeros([len(x),3])
 X[:,0] = 1
 X[:,1] = x[:,0]
 X[:,2] = x[:,0]**2
-X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2)
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.3)
 
 beta = np.linalg.inv(X_train.T @ X_train) @ X_train.T @ y_train
-print(beta)
+
+ytilde_train = X_train @ beta
+
+MSE_trainval = metric.mean_squared_error(y_train,ytilde_train)
+R2_trainval = metric.r2_score(y_train,ytilde_train)
+
+ytilde_test = X_test @ beta
+
+MSE_testval = metric.mean_squared_error(y_test,ytilde_test)
+R2_testval = metric.r2_score(y_test,ytilde_test)
+
+print("MSE Train:")
+print(MSE_trainval)
+print("R2 Train:")
+print(R2_trainval)
+
+print("---------------------")
+
+print("MSE Test:")
+print(MSE_testval)
+print("R2 Test:")
+print(R2_testval)
 
 plt.plot(x,y,"o")
 plt.plot(x_poly,poly2(parametrization),"--")
