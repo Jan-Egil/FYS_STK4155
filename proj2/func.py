c2530ea22c3181209bb9702b3442e1291a077dc7
George Cowie <gecocowie@live.com>
2020-10-21 15:30:02 +0200

    SGD funker?

--- proj2/func.py
+++ proj2/func.py
@@ -13,9 +13,63 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.utils import resample
 from sklearn.linear_model import Lasso, Ridge, SGDRegressor
 
+def DesignMatrixCreator_1dpol(p,x):
+    """
+    Creates a design matrix for 1 variable polynomial x
 
+    INPUT:
+    p: Degree of polynomial
+    x : Array of x-values
 
-def SGD(X,n,M,epochs):
+    OUTPUT:
+    X: Design matrix
+    """
+
+    if len(x.shape) > 1:
+        x = np.ravel(x)
+
+    N = len(x)
+    X = np.zeros([N,p])
+    X[:,0] = 1
+    column = 0
+    for i in range(p):
+        X[:,i] = (x**i)
+        column += 1
+    return X
+
+def OLS(XTrain, XTest, yTrain, yTest):
+    """
+    Performs an Ordinary Least Squares (OLS) regression on a data set
+
+    INPUT:
+    XTrain: The training design matrix of the problem
+    XTest: The testing design matrix of the problem
+    yTrain: The data point sets corresponding to the respective points in the training design matrix
+    yTest: The data point sets corresponding to the respective points in the test design matrix
+
+    OUTPUT:
+    ytildeTest: Approximated values corresponding to the test data
+    ytildeTrain: Approximated values corresponding to the train data
+    Beta_OLS_optimal: The optimal coefficient values for the best-fit polynomial in OLS
+    """
+
+    Beta_OLS_optimal = np.linalg.pinv(XTrain.T @ XTrain) @ XTrain.T @ yTrain
+
+    ytildeTrain = XTrain @ Beta_OLS_optimal
+    ytildeTest = XTest @ Beta_OLS_optimal
+
+    return ytildeTest, ytildeTrain, Beta_OLS_optimal
+
+def learning_schedule(t):
+    """
+    Learning schedule that gradually reduces learning rate
+    Input:
+    t = epochs*m+i
+    """
+    t0,t1 = 5,50
+    return t0/(t+t1)
+
+def SGD(X,y,n,M,epochs):
     """
     Stochastic Gradient Descent
     Input:
@@ -25,16 +79,33 @@ def SGD(X,n,M,epochs):
     epochs = number of iterations over minibatches
     """
     m = int(n/M)
-    for i in range(1,epochs+1):
+    theta = np.random.randn(2,1) # random initialization
+    for epoch in range(epochs):
         for j in range(m):
-            k = np.random.randint(m)
-            gradient = 2/n*X.T@(X@beta-y)#Derivative of cost function
+            k = np.random.randint(m)#index to pick random bin
+            Xk = X[k:k+1]
+            yk = y[k:k+1]
+            gradient = 2*Xk.T@(Xk@theta-yk)#Derivative of cost function
+            gamma = learning_schedule(epoch * m + j)
+            theta = theta - gamma * gradient
+    return(theta)
+
+    """Kode er i stor grad basert på Geron sin tekstbok. """
 
 
-def learning_schedule(t):
-    """
-    Learning schedule that gradually reduces learning rate
-    Input:
-    t = epochs*m+i
-    """
-    return t0/(t+t1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
--- proj2/main.py
+++ proj2/main.py
@@ -2,16 +2,58 @@
 from func import *#importing everything from func.py, including external packages
 
 print("\n\nWhich task do you want to run?")
-exercise = input("Press any letter between a & g: ")
+exercise = 'a'#input("Press any letter between a & g: ")
 
 """
 Part a)
 """
 
 if exercise == "a":
-    M = 5#Minibatch size
+
     n = 100#number of datapoints
-    m = n/M
-    epochs = 10
+
+    x = 2 * np.random.rand(n, 1)
+    y = 4 + 3 * x + np.random.randn(n, 1)
+
+    X = DesignMatrixCreator_1dpol(2,x) #Create design matrix
+
+    """theta from own OLS"""
+    y_,y_tild,theta_ols = OLS(X,X,y,y)
+    print('Theta from OLS: ', theta_ols,'\n')
+    """sklearns SGD"""
+    sgd_reg = SGDRegressor(max_iter=1000, penalty=None, eta0=0.1)
+    sgd_reg.fit(x, y.ravel())
+    print('Theta from sklearn SGD: ',sgd_reg.intercept_,' ', sgd_reg.coef_,'\n')
+
+    """Own SGD scheme"""
+    M = 1#Minibatch size
+    #m = int(n/M)
+    epochs = 50
     Tolerance = 1e-10
-    
+
+    theta_own_SGD = SGD(X,y,n,M,epochs)
+
+    print('Theta from own SGD: ',theta_own_SGD)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
