5391edc5487079164856dd6b8bfe2cc41fb4f294
George Cowie <gecocowie@live.com>
2020-10-22 14:17:52 +0200

    saktesakte men sikkert skjer det ting her

--- proj2/func.py
+++ proj2/func.py
@@ -37,6 +37,37 @@ def DesignMatrixCreator_1dpol(p,x):
         column += 1
     return X
 
+def DesignMatrixCreator_2dpol(p,x,y):
+    """
+    Creates a design matrix for 2 variable polynomial (x,y)
+
+    INPUT:
+    p: Degree of polynomial
+    x, y: Array of x-values & y-values
+
+    OUTPUT:
+    X: Design matrix
+    """
+
+    if len(x) != len(y):
+        sys.exit(0)
+
+    if len(x.shape) > 1:
+        x = np.ravel(x)
+        y = np.ravel(y)
+
+    N = len(x)
+    num_of_terms = int(((p+1)*(p+2))/2)
+
+    X = np.zeros([N,num_of_terms])
+    X[:,0] = 1
+    column = 1
+    for i in range(1,p+1):
+        for j in range(i+1):
+            X[:,column] = (x**j)*(y**(i-j))
+            column += 1
+    return X
+
 def OLS(XTrain, XTest, yTrain, yTest):
     """
     Performs an Ordinary Least Squares (OLS) regression on a data set
@@ -59,6 +90,49 @@ def OLS(XTrain, XTest, yTrain, yTest):
     ytildeTest = XTest @ Beta_OLS_optimal
 
     return ytildeTest, ytildeTrain, Beta_OLS_optimal
+
+def frankefunc_noise(x,y,noise):
+    """
+    The Franke function evaluated at a given point (x,y) including noise.
+
+    INPUT:
+    x,y: position (x,y)
+    noise: Factor of noise
+
+    OUTPUT:
+    Franke Function value at position (x,y) with added noise
+    """
+    if len(x) != len(y):
+        sys.exit(0)
+
+    if len(x.shape) > 1:
+        x = np.ravel(x)
+        y = np.ravel(y)
+
+    N = len(x)
+    term1 = 0.75*np.exp(-((9*x-2)**2)/4 - ((9*x-2)**2)/4)
+    term2 = 0.75*np.exp(-((9*x+1)**2)/49 - (9*y+1)/10)
+    term3 = 0.5*np.exp(-((9*x-7)**2)/4 - ((9*y-3)**2)/4)
+    term4 = 0.2*np.exp(-(9*x-4)**2 - (9*y-7)**2)
+    return term1 + term2 + term3 - term4 + noise*np.random.randn(N)
+
+def scale(xtrain, xtest):
+    """
+    Scales the data using the StandardScaler from SKLearn.
+
+    INPUT:
+    xtrain, xtest: Unscaled design matrices (We scale using the first input)
+
+    OUTPUT:
+    xtrain_scaled, xtest_scaled: Scaled design matrices
+    """
+
+    scaler = StandardScaler()
+    scaler.fit(xtrain)
+    xtrain_scaled = scaler.transform(xtrain); xtrain_scaled[:,0] = 1
+    xtest_scaled = scaler.transform(xtest); xtest_scaled[:,0] = 1
+
+    return xtrain_scaled, xtest_scaled
 
 def learning_schedule(t):
     """
@@ -83,8 +157,8 @@ def SGD(X,y,n,M,epochs):
     for epoch in range(epochs):
         for j in range(m):
             k = np.random.randint(m)#index to pick random bin
-            Xk = X[k:k+1]
-            yk = y[k:k+1]
+            Xk = X[k:k+M]
+            yk = y[k:k+M]
             gradient = 2*Xk.T@(Xk@theta-yk)#Derivative of cost function
             gamma = learning_schedule(epoch * m + j)
             theta = theta - gamma * gradient
@@ -92,11 +166,47 @@ def SGD(X,y,n,M,epochs):
 
     """Kode er i stor grad basert på Geron sin tekstbok. """
 
+def logistic_sigmoid(x):
+    return 1/(1+np.exp(-x))
+
+def activation_func(x,w,b):
+    """
+    input:
+    1. x = function input
+    2. w = wheights
+    3. b = bias
+    """
+    z = x @ w + b
+    return logistic_sigmoid(z)
+
+def FFNN(x,N_n,N_l,*args):
+    """
+    input:
+    x = function input
+    N_n = nodes in layer
+    N_l = Number of layers
+    Optional input args:
+    1. w = wheights
+    2. b = bias
+    """
+    if len(args) == 2:
+        w = args[0]
+        b = args[1]
+
+    if (args) == ():
+        w = np.random.rand(len(x),N_l)
+        b = np.random.rand(N_l)
+
+
+    for i in range(N_l):
+        Z = activation_func(x,w,b)
+
+    y = 1
+    return y
 
 
 
-
-
+FFNN([1,2,3],4,1)
 
 
 
--- proj2/main.py
+++ proj2/main.py
@@ -2,7 +2,7 @@
 from func import *#importing everything from func.py, including external packages
 
 print("\n\nWhich task do you want to run?")
-exercise = 'a'#input("Press any letter between a & g: ")
+exercise = 'b'#input("Press any letter between a & g: ")
 
 """
 Part a)
@@ -14,12 +14,12 @@ if exercise == "a":
 
     x = 2 * np.random.rand(n, 1)
     y = 4 + 3 * x + np.random.randn(n, 1)
-
     X = DesignMatrixCreator_1dpol(p,x) #Create design matrix
 
     """theta from own OLS"""
     y_xx,y_gg,theta_ols = OLS(X,X,y,y)
     print('Theta from OLS: ', theta_ols,'\n')
+
     """sklearns SGD"""
     sgd_reg = SGDRegressor(max_iter=1000, penalty=None, eta0=0.1)
     sgd_reg.fit(x, y.ravel())
@@ -27,14 +27,26 @@ if exercise == "a":
 
     """Own SGD scheme"""
     M = 1#Minibatch size
-    #m = int(n/M)
     epochs = 50
     Tolerance = 1e-10
-
     theta_own_SGD = SGD(X,y,n,M,epochs)
-
     print('Theta from own SGD: ',theta_own_SGD)
 
+if exercise == 'b':
+    N = 200 #Number of data points
+    polydeg = 2#int(input("Enter the degree of polynomial you want to approximate: ")) #Order of polynomial
+    noise = 0.2 #Factor of noise in data
+
+    xy = np.random.rand(N,2) #Create random function parameters
+    x = xy[:,0]; y = xy[:,1]
+
+
+    X = DesignMatrixCreator_2dpol(polydeg,x,y) #Create design matrix
+
+    z = frankefunc_noise(x,y,noise) #
+
+    X_train, X_test, zTrain, zTest = train_test_split(X,z,test_size=0.2) #Split data into training and testing set
+    X_train, X_test = scale(X_train, X_test) #Properly scale the data
 
 
 
