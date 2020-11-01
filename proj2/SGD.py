SGD function properly fixed (missed normalization factor)

proj2/SGD.py
@@ -10,9 +10,9 @@ polydegs = np.arange(1,n_poly+1)
 
 for n in polydegs:
     print(n)
-    N = 1000 #Number of data points
+    N = 100 #Number of data points
     polydeg = n #Order of polynomial
-    noise = 0.2 #Factor of noise in data
+    noise = 0.5 #Factor of noise in data
 
     xy = np.random.rand(N,2) #Create random function parameters
     x = xy[:,0]; y = xy[:,1]
@@ -25,23 +25,30 @@ for n in polydegs:
     X_train, X_test, zTrain, zTest = train_test_split(X,z,test_size=0.2) #Split data into training and testing set
     X_train, X_test = scale(X_train, X_test) #Properly scale the data
 
-    """theta from own OLS"""
+    """
+    theta from own OLS
+    """
 
     z_tilde_test, z_tilde_train, theta_ols = OLS(X_train, X_test, zTrain, zTest)
     #y_xx,y_gg,theta_ols = OLS(X,X,y,y)
     #print('\nTheta from OLS: ', theta_ols,'\n')
 
-    """sklearns SGD"""
+    """
+    sklearns SGD
+    """
 
-    sgd_reg = SGDRegressor(max_iter=50, penalty=None, eta0=0.1)
+    sgd_reg = SGDRegressor(max_iter=100, penalty='l2', eta0=0.1)
     #sgd_reg.fit(x, y.ravel())
     a = sgd_reg.fit(X_train, zTrain)
     #print('Theta from sklearn SGD: ', a.coef_,'\n')
 
-    """Own SGD scheme"""
+    """
+    Own SGD scheme
+    """
+
     M = 2 #Minibatch size
-    epochs = 50
-    Tolerance = 1e-12
+    epochs = 100
+    Tolerance = 1e-10
 
     theta_own_SGD = SGD(X_train,zTrain,N,M,epochs)
 
@@ -49,7 +56,7 @@ for n in polydegs:
 
     MSE_OLS = metric.mean_squared_error(zTest,z_tilde_test)
     MSE_SGD_own = metric.mean_squared_error(zTest,X_test@theta_own_SGD)
-    MSE_SGD_SKLearn = metric.mean_squared_error(zTest,X_test@a.coef_)
+    MSE_SGD_SKLearn = metric.mean_squared_error(zTest,sgd_reg.predict(X_test))
 
     MSE_OLS_array[n-1] = MSE_OLS
     MSE_SGD_array[n-1] = MSE_SGD_own

proj2/func.py
@@ -166,7 +166,7 @@ def SGD(X,y,n,M,epochs):
             k = np.random.randint(m) #Index to pick random bin
             X_k = X[k:k+M]
             y_k = y[k:k+M]
-            gradient = 2*X_k.T@((X_k@theta)-y_k) #Derivative of cost function
+            gradient = (2/n)*X_k.T@((X_k@theta)-y_k) #Derivative of (MSE) cost function
             gamma = learning_schedule(epoch * m + j)
             theta = theta - gamma * gradient
     return theta