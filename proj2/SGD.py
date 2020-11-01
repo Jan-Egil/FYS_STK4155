Overflow issues with our own SGD. Probably something wrong..

proj2/SGD.py
@@ -1,46 +1,65 @@
 from func import * #importing everything from func.py, including external packages.
 
-#This is the solution to part a of project 2
+#This is the solution to part a) of project 2
 
-N = 200 #Number of data points
-polydeg = 2#int(input("Enter the degree of polynomial you want to approximate: ")) #Order of polynomial
-noise = 0.2 #Factor of noise in data
+n_poly = 18
+MSE_OLS_array = np.zeros(n_poly)
+MSE_SGD_array = np.zeros(n_poly)
+MSE_SGD_sklearn_array = np.zeros(n_poly)
+polydegs = np.arange(1,n_poly+1)
 
-xy = np.random.rand(N,2) #Create random function parameters
-x = xy[:,0]; y = xy[:,1]
+for n in polydegs:
+    print(n)
+    N = 1000 #Number of data points
+    polydeg = n #Order of polynomial
+    noise = 0.2 #Factor of noise in data
+
+    xy = np.random.rand(N,2) #Create random function parameters
+    x = xy[:,0]; y = xy[:,1]
 
 
-X = DesignMatrixCreator_2dpol(polydeg,x,y) #Create design matrix
+    X = DesignMatrixCreator_2dpol(polydeg,x,y) #Create design matrix
 
-z = frankefunc_noise(x,y,noise) #
+    z = frankefunc_noise(x,y,noise) #Corresponding Franke Function val w/ noise
 
-X_train, X_test, zTrain, zTest = train_test_split(X,z,test_size=0.2) #Split data into training and testing set
-X_train, X_test = scale(X_train, X_test) #Properly scale the data
+    X_train, X_test, zTrain, zTest = train_test_split(X,z,test_size=0.2) #Split data into training and testing set
+    X_train, X_test = scale(X_train, X_test) #Properly scale the data
 
-#p = 2
-#n = 100#number of datapoints
+    """theta from own OLS"""
 
-#x = 2 * np.random.rand(n, 1)
-#y = 4 + 3 * x + np.random.randn(n, 1)
-#X = DesignMatrixCreator_1dpol(p,x) #Create design matrix
+    z_tilde_test, z_tilde_train, theta_ols = OLS(X_train, X_test, zTrain, zTest)
+    #y_xx,y_gg,theta_ols = OLS(X,X,y,y)
+    #print('\nTheta from OLS: ', theta_ols,'\n')
 
-"""theta from own OLS"""
+    """sklearns SGD"""
 
-z_tilde_test, z_tilde_train, theta_ols = OLS(X_train, X_test, zTrain, zTest)
-#y_xx,y_gg,theta_ols = OLS(X,X,y,y)
-print('Theta from OLS: ', theta_ols,'\n')
+    sgd_reg = SGDRegressor(max_iter=50, penalty=None, eta0=0.1)
+    #sgd_reg.fit(x, y.ravel())
+    a = sgd_reg.fit(X_train, zTrain)
+    #print('Theta from sklearn SGD: ', a.coef_,'\n')
 
-"""sklearns SGD"""
+    """Own SGD scheme"""
+    M = 2 #Minibatch size
+    epochs = 50
+    Tolerance = 1e-12
 
-sgd_reg = SGDRegressor(max_iter=1000, penalty=None, eta0=0.1)
-#sgd_reg.fit(x, y.ravel())
-sgd_reg.fit(X_train, zTrain.ravel())
-print('Theta from sklearn SGD: ', sgd_reg.coef_,'\n')
+    theta_own_SGD = SGD(X_train,zTrain,N,M,epochs)
 
-"""Own SGD scheme"""
-M = 2#Minibatch size
-epochs = 50
-Tolerance = 1e-10
-print(X_train.shape[1])
-theta_own_SGD = SGD(X_train,zTrain,N,M,epochs)
-print('Theta from own SGD: ',theta_own_SGD)
+    #print('Theta from own SGD: ',theta_own_SGD)
+
+    MSE_OLS = metric.mean_squared_error(zTest,z_tilde_test)
+    MSE_SGD_own = metric.mean_squared_error(zTest,X_test@theta_own_SGD)
+    MSE_SGD_SKLearn = metric.mean_squared_error(zTest,X_test@a.coef_)
+
+    MSE_OLS_array[n-1] = MSE_OLS
+    MSE_SGD_array[n-1] = MSE_SGD_own
+    MSE_SGD_sklearn_array[n-1] = MSE_SGD_SKLearn
+
+
+plt.plot(polydegs,MSE_OLS_array,label="OLS")
+plt.plot(polydegs,MSE_SGD_array,label="SGD")
+plt.plot(polydegs,MSE_SGD_sklearn_array,label="SKLearn")
+plt.grid(); plt.legend(); plt.semilogy()
+plt.xlabel("Complexity of model")
+plt.ylabel("MSE")
+plt.show()

proj2/func.py
@@ -137,8 +137,12 @@ def scale(xtrain, xtest):
 def learning_schedule(t):
     """
     Learning schedule that gradually reduces learning rate
-    Input:
+
+    INPUT:
     t = epochs*m+i
+
+    OUTPUT:
+    Learning schedule
     """
     t0,t1 = 5,50
     return t0/(t+t1)
@@ -156,13 +160,13 @@ def SGD(X,y,n,M,epochs):
     "Optimal" parameters
     """
     m = int(n/M)
-    theta = np.random.randn(X.shape[1]) # random initialization
+    theta = np.random.randn(X.shape[1]) #Random initialization
     for epoch in range(epochs):
         for j in range(m):
-            k = np.random.randint(m)#index to pick random bin
-            Xk = X[k:k+M]
-            yk = y[k:k+M]
-            gradient = 2*Xk.T@(Xk@theta-yk)#Derivative of cost function
+            k = np.random.randint(m) #Index to pick random bin
+            X_k = X[k:k+M]
+            y_k = y[k:k+M]
+            gradient = 2*X_k.T@((X_k@theta)-y_k) #Derivative of cost function
             gamma = learning_schedule(epoch * m + j)
             theta = theta - gamma * gradient
     return theta